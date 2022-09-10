66f2cf4cf9f29c5fad434cc6d54817609ede86d4
Eric McNiece <emcniece@gmail.com>
2022-09-10 16:00:18 -0700

    wip

--- woo-accounting-sync.php
+++ woo-accounting-sync.php
@@ -14,9 +14,8 @@ defined( 'ABSPATH' ) or die( 'No direct execution' );
 
 class WASync{
   public function __construct(){
-    $this->constants();
-
-    require_once(AKAWOO_PATH . 'library/helper.php');
+    //$this->constants();
+    //require_once(AKAWOO_PATH . 'library/helper.php');
 
     $this->init();
   }
@@ -28,11 +27,16 @@ class WASync{
         // to do
     } elseif (!isset($wp_customize)) {
       add_action('admin_menu', array($this, 'menu'));
+      add_action('admin_init', array($this, 'register_options'));
     }
   }
 
   public function menu(){
     add_options_page('WooCommerce Akaunting Sync Settings', 'Akaunting WooCommerce', 'manage_options', 'akaunting-woocommerce', array($this, 'display_options'));
+  }
+
+  public function register_options(){
+    register_setting('wasync-options', 'wasync_url');
   }
 
   public function display_options(){
